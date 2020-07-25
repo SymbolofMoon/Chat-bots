d78aed17dd4a6b0efcbcb3a5972ecb8d71536f15
prateekchandra <prateekchandra1027@gmail.com>
2020-07-25 15:06:03 +0530

    Completed Chatbot

--- linkedin_chatbot.py
+++ linkedin_chatbot.py
@@ -31,7 +31,7 @@ profilesQueued = []
 
 def getNewProfileIDs(soup, profilesQueued):
     profilesID=[]
-    pav = soup.find('div',{'class': 'pv-browsemap-section pv-browsemap-section__wrapper'})
+    pav = soup.find('section',{'class': 'pv-browsemap-section pv-browsemap-section__wrapper'})
     all_links = pav.findAll('a',{'class':'pv-browsemap-section__member ember-view'})
     for link in all_links:
         userID = link.get('href')
@@ -46,6 +46,52 @@ def getNewProfileIDs(soup, profilesQueued):
 profilesQueued = getNewProfileIDs(BeautifulSoup(browser.page_source, "lxml"), profilesQueued)            
 
 while profilesQueued:
-    visitingProfileID = profilesQueued.pop()
-    visitedProfiles.append(visitingProfileID)
-    
+    try:
+        visitingProfileID = profilesQueued.pop()
+        visitedProfiles.append(visitingProfileID)
+        fullLink = 'https://www.linkedin.com' + visitingProfileID
+        browser.get(fullLink)
+        browser.find_element_by_class_name("pv-s-profile-actions ").click()
+        browser.find_element_by_class_name("mr1").click()
+
+        customMessage= "Hello, It's my pleasure to connect with you!!!"
+        elementID = browser.find_element_by_id('custom-message')
+        elementID.send_keys(customMessage)
+
+        browser.find_element_by_class_name("ml1").click()
+
+       #Add the ID to the visitedusersfile
+
+        with open('visitedUsers.txt','a') as visitedUsersFile:
+            visitedUsersFile.write(str(visitingProfileID) + '\n')
+
+        visitedUsersFile.close()
+
+       #Get new profiles ID
+        soup = BeautifulSoup(browser.page_source)
+        try:
+            profilesQueued.extend(getNewProfileIDs(soup, profilesQueued))
+        except:
+             print('continue')
+
+        #Pause
+        time.sleep(random.uniform(5, 15))
+
+        if(len(visitedProfiles)%50==0):
+             print("Visited profiles: ", len(visitedProfiles))
+        
+        if(len(profilesQueued)> 100000):
+            with open('profileQueued.txt','a') as visitedUsersFile:
+                visitedUsersFile.write(str(visitingProfileID) + '\n')
+
+            visitedUsersFile.close()    
+            print('100000 Done!!!!!')
+            break
+
+    except:
+        print("error")    
+        
+
+      
+        
+         

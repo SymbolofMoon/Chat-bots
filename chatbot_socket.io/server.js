9456dde6c194501e148853cb57c19ac55a1c5cad
prateekchandra <prateekchandra1027@gmail.com>
2020-12-21 19:58:49 +0530

    Installed and setup the socket.io

--- chatbot_socket.io/server.js
+++ chatbot_socket.io/server.js
@@ -16,3 +16,11 @@ app.use(express.static(__dirname+'/public'))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
+
+
+const io = require('socket.io')(http)
+
+
+io.on('connection', (socket) => {
+    console.log("Connected...")
+})
